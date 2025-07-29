f59a23c363c36458f260bd0f0c7912a09657b6d2
Joey Rudd <emailjoeyrudd@gmail.com>
2025-07-29 19:34:11 -0400

    Implement config::new function to replace old parse_config function to improve readability

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, file_path) = parse_config(&args);
+    let config = Config::new(&args);
 
-    println!("Searching for {query}");
-    println!("In the file path {file_path}");
+    println!("Searching for {}", config.query);
+    println!("In the file path {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
 }
@@ -17,9 +18,12 @@ struct Config {
     query: String,
     file_path: String,
 }
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
 
-    Config { query, file_path }
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path }
+    }
 }
